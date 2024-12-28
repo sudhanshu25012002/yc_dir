ee28d1789ec065f81526687764679cff38045311
Sudhanshu Ranjan <sr25012002@gmail.com>
2024-12-28 11:16:38 +0530

    cmmit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+            </nav>
+            
+        </header>
+    )
+}
+export default Navbar
